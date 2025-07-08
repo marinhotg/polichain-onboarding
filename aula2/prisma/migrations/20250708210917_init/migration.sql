6ad7c13b0b2532f0d68e2625bcccb8f604df9088
marinhotg <tiago.mlima@hotmail.com>
2025-07-08 18:15:16 -0300

    using sqlite

---
+++ aula2/prisma/migrations/20250708210917_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
